f71ed40c0a96323bc587e6589e5d5989a503203a
Dennis Wegereef <dennis.wegereef@deptagency.com>
2023-06-20 14:01:33 +0200

    Update build

---
+++ src/components/resonating-letters/settings.ts
@@ -0,0 +1,23 @@
+interface Settings {
+  slices: {
+    margin: number;
+    clipPath?: string;
+  }[];
+}
+
+export const SETTINGS_REGULAR_RIGHT: Settings = {
+  slices: [
+    {
+      margin: 0,
+      clipPath: "polygon(-50% -50%, 50% 0%, 30% 100%, 0% 100%)",
+    },
+    {
+      margin: 13,
+      clipPath: "polygon(40% 0%, 50% 0%, 65% 100%, 20% 100%)",
+    },
+    {
+      margin: 55,
+      clipPath: "polygon(10% 0%, 100% 0%, 100% 100%, 25% 100%)",
+    },
+  ],
+};
