8d5f19d51ac72eb92fac3b38176b2cfe9d43ff07
Dennis Wegereef <dennis.wegereef@deptagency.com>
2023-06-20 14:41:05 +0200

    Update clip path

--- src/components/resonating-letters/settings.ts
+++ src/components/resonating-letters/settings.ts
@@ -9,15 +9,15 @@ export const SETTINGS_REGULAR_RIGHT: Settings = {
   slices: [
     {
       margin: 0,
-      clipPath: "polygon(-50% -50%, 50% 0%, 30% 100%, 0% 100%)",
+      clipPath: "polygon(0% 0%, 40% 0%, 40% 100%, 0% 100%)",
     },
     {
-      margin: 13,
-      clipPath: "polygon(40% 0%, 50% 0%, 65% 100%, 20% 100%)",
+      margin: 10,
+      clipPath: "polygon(40% 0%, 75% 0%, 85% 100%, 30% 100%)",
     },
     {
-      margin: 55,
-      clipPath: "polygon(10% 0%, 100% 0%, 100% 100%, 25% 100%)",
+      margin: 30,
+      clipPath: "polygon(65% 0%, 100% 0%, 100% 100%, 75% 100%)",
     },
   ],
 };
@@ -29,11 +29,11 @@ export const SETTINGS_REGULAR_LEFT: Settings = {
       clipPath: "polygon(-50% -50%, 50% 0%, 30% 100%, 0% 100%)",
     },
     {
-      margin: 13,
+      margin: 10,
       clipPath: "polygon(40% 0%, 50% 0%, 65% 100%, 20% 100%)",
     },
     {
-      margin: 55,
+      margin: 45,
       clipPath: "polygon(10% 0%, 100% 0%, 100% 100%, 25% 100%)",
     },
   ],
