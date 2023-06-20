fc3da0992b0b1f07e48e3027335237bd1564bb0a
Dennis Wegereef <dennis.wegereef@deptagency.com>
2023-06-20 18:52:05 +0200

    Add back adapter vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import glsl from "vite-plugin-glsl";
-import node from "@astrojs/node";
-
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [glsl()]
+    plugins: [glsl()],
   },
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel(),
 });
