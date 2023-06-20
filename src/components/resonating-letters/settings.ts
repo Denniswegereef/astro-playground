38a99d69507b1d9cf45c56c5ce4689927d092349
Dennis Wegereef <dennis.wegereef@deptagency.com>
2023-06-20 14:28:54 +0200

    Fix scaling

--- src/components/resonating-letters/settings.ts
+++ src/components/resonating-letters/settings.ts
@@ -21,3 +21,20 @@ export const SETTINGS_REGULAR_RIGHT: Settings = {
     },
   ],
 };
+
+export const SETTINGS_REGULAR_LEFT: Settings = {
+  slices: [
+    {
+      margin: 0,
+      clipPath: "polygon(-50% -50%, 50% 0%, 30% 100%, 0% 100%)",
+    },
+    {
+      margin: 13,
+      clipPath: "polygon(40% 0%, 50% 0%, 65% 100%, 20% 100%)",
+    },
+    {
+      margin: 55,
+      clipPath: "polygon(10% 0%, 100% 0%, 100% 100%, 25% 100%)",
+    },
+  ],
+};
